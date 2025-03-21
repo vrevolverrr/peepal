c696ff8d9e049318ccd1455ccbacaeb79e0e89aa
Bryan Soong <whiterevolver35@gmail.com>
2025-03-21 14:07:39 +0800

    fixed ios google map config

--- peepal/ios/Runner/AppDelegate.swift
+++ peepal/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCcO6Un_kJwFTHbsvPcSaTFbaovCX3F10g")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }

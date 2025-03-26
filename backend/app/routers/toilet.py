59c4fd2c97340fb90439acc76a37554f442f2ac3
Liew Jia Wei <mjw.liew@gmail.com>
2025-03-26 23:05:49 +0800

    finished createToilet flow

--- backend/app/routers/toilet.py
+++ backend/app/routers/toilet.py
@@ -3,13 +3,13 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from db import get_db_connection
+from db import get_db
 
 from controllers.toilet import ToiletController ##
 import schemas.toilet as toilet_schemas ##
 
 router = APIRouter()
 
-@router.post("/api/toilets", response_model = list[toilet_schemas.Toilet]) 
-def add_toilet(toilet: toilet_schemas.ToiletCreate, db: Session = Depends(get_db_connection)):
+@router.post("/api/toilets", response_model = toilet_schemas.Toilet) 
+def add_toilet(toilet: toilet_schemas.ToiletCreate, db: Session = Depends(get_db)):
     return ToiletController.createToilet(db, toilet)

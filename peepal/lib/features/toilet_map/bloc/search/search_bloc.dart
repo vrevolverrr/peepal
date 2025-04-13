760a671774dd386500b2e361fd9a230b4b7b47bb
Bryan Soong <whiterevolver35@gmail.com>
2025-04-14 00:30:58 +0800

    added searchbloc

--- peepal/lib/features/toilet_map/bloc/search/search_bloc.dart
+++ peepal/lib/features/toilet_map/bloc/search/search_bloc.dart
@@ -1,1 +1,17 @@
+import 'package:equatable/equatable.dart';
 
+import 'package:equatable/equatable.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:peepal/api/toilets/model/search_results.dart';
+
+part 'search_state.dart';
+part 'search_event.dart';
+
+class SearchBloc extends Bloc<SearchEvent, SearchState> {
+  SearchBloc() : super(SearchStateInitial()) {
+    on<SearchEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
--- peepal/lib/features/toilet_map/bloc/search/search_state.dart
+++ peepal/lib/features/toilet_map/bloc/search/search_state.dart
@@ -1,5 +1,24 @@
 part of 'search_bloc.dart';
 
 sealed class SearchState extends Equatable {
-  const SearchState();
+  final List<PPSearchResult> results;
+
+  const SearchState({this.results = const []});
+
+  @override
+  List<Object?> get props => [results];
+}
+
+final class SearchStateInitial extends SearchState {
+  const SearchStateInitial();
+}
+
+final class SearchStateLoading extends SearchState {
+  const SearchStateLoading();
+}
+
+final class SearchStateError extends SearchState {
+  final String error;
+
+  const SearchStateError({required this.error});
 }
